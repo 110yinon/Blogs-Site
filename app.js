8e8c971c22aaf8c596e3ce66c18604c9ff03e736
goo <goo>
2022-04-13 20:29:49 +0300

    db connection

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 // express app
 const app = express();
@@ -7,24 +10,46 @@ const app = express();
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000, () => console.log('listening on port 3000'));
+// listen for requests & db connection
+mongoose.connect('mongodb://localhost:27017/myDB')
+    .then(result => app.listen(3000, () => console.log('listening on port 3000')))
+    .catch(err => console.log(err))
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 
-// db object
-const blogs = [
-    { id: 1, title: 'bb the king', snippet: 'bb is the king', body: 'lorem loremlorem lorem lorem loremlorem loremlorem loremlorem loremlorem lorem' },
-    { id: 2, title: 'sara', snippet: 'sara the queen of balfur', body: 'lorem loremlorem lorem lorem loremlorem loremlorem loremlorem loremlorem lorem' },
-    { id: 3, title: 'kuni isra flag', snippet: 'some kuni with israel flag', body: 'lorem lorem lorem loremlorem loremlorem loremlorem loremlorem loremlorem lorem' },
-    { id: 4, title: 'kuni lamel', snippet: 'a kuni man with a briefcase', body: 'lorem lorem lorem loremlorem loremlorem loremlorem loremlorem lorem' }
-];
-
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home', blogs });
+    Blog.find()
+        .then(result => {
+            console.log(result);
+            res.render('index', { title: 'Home', blogs: result });
+        })
+        .catch(err => console.log(err))
+});
+
+app.get('/blogs', (req, res) => {
+    res.redirect('/');
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(result => {
+            console.log('blog added:\n', result);
+            res.redirect('/');
+        })
+        .catch(err => console.log(err))
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id)
+        .then(result => {
+            console.log(result);
+            res.json({ redirect: '/' });
+        })
+        .catch(err => console.log(err))
 });
 
 app.get('/about', (req, res) => {
@@ -35,10 +60,17 @@ app.get('/blog', (req, res) => {
     res.render('create', { title: 'Create' });
 });
 
-app.get('/blogs/:id', (req, res) => {
-    console.log(req.params.id);
-    const blog = blogs.filter(blog => blog.id == req.params.id);
-    res.render('blog', { title: 'Blog', blog:blog[0] });
+app.get('/blogs/:id', (req, res, next) => {
+    Blog.findById(req.params.id)
+        .then(result => {
+            console.log(result);
+            res.render('blog', { title: 'Blog', blog: result });
+        })
+        .catch(err => {
+            console.log(err);
+            next();
+        })
+
 });
 
 app.use((req, res) => {
